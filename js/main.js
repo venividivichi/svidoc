aa1e10f2fc8fee2aab0f5c104edb32ea6a8527d4
venividivichi <moloduy23@gmail.com>
2018-11-19 21:42:05 +0200

    all changes

--- js/main.js
+++ js/main.js
@@ -1,74 +1,326 @@
 
-var infoContainer = document.getElementById("print_out");
-var btn = document.getElementById("btn");
+
+const infoContainer = document.getElementById("print_out");
+const btn = document.getElementById("btn");
+const tit = document.getElementById("try_hide");
 
 
-btn.addEventListener("click", function() {
-    var vvod = $('#vvvod').val();
-    var myRequest = new XMLHttpRequest();
+/*var form = document.querySelector('#myForm');
+var submit_js_btn = document.querySelector('#js_submit');
 
-    myRequest.open('GET', 'http://10.30.1.208:8080/nais-exchange/resources/cars/v1/cert/' + vvod);
-    myRequest.onload = function() {
 
-        if (myRequest.status != 200) {
-            alert(myRequest.status + ': ' + myRequest.statusText);
-            document.location.href='notfound.html';
-        } else {
-            var myData = JSON.parse(myRequest.responseText);
-            console.log(myData[5]);
-            renderHTML(myData);
-            /*console.log(myData[0]);*/
+            submit_js_btn.addEventListener('click', function () {
+
+
+                  const vvod = $('#vvvod').val();
+                  const vvod2 = $('#vvvod2').val();
+
+
+
+                    if (vvod == '' || vvod2 =='') {
+                           document.getElementById("nameErrMsg").innerHTML = " Введіть щось в поля";
+                           document.getElementById("vvvod").setAttribute('style', 'border: 2px solid #ff0000');
+                           document.getElementById("vvvod2").setAttribute('style', 'border: 2px solid #ff0000');
+                           return false
+                    }
+
+                    fetch(`http://10.30.1.208:8080/nais-exchange/resources/cars/RegisterVehiclesOwners/GetTechnicalPassport/${vvod}/${vvod2}`)
+
+                      .then(response => {
+
+                        if (response.ok) {
+                            return response.json();
+
+                        }else {
+
+                           return window.location.href='http://hsc.gov.ua/svidocnotfound/';
+                        }
+                       })
+                        .then(renderHTML)
+
+                        .catch(err => console.log('Error, with message:', err.statusText))
+
+
+            }, false);*/
+
+
+/* function press_btn() {
+
+       const vvod = $('#vvvod').val();
+       const vvod2 = $('#vvvod2').val();
+
+
+
+       if (vvod == '' || vvod2 =='') {
+              document.getElementById("nameErrMsg").innerHTML = " Введіть щось в поля";
+              document.getElementById("vvvod").setAttribute('style', 'border: 2px solid #ff0000');
+              document.getElementById("vvvod2").setAttribute('style', 'border: 2px solid #ff0000');
+              return false
+       }
+
+       fetch(`http://10.30.1.208:8080/nais-exchange/resources/cars/RegisterVehiclesOwners/GetTechnicalPassport/${vvod}/${vvod2}`)
+
+         .then(response => {
+
+           if (response.ok) {
+               return response.json();
+
+           }else {
+
+              return window.location.href='http://hsc.gov.ua/svidocnotfound/';
+           }
+          })
+           .then(renderHTML)
+
+           .catch(err => console.log('Error, with message:', err.statusText))
+
+           return false;
+   }
+*/
+
+$("#myForm").on('submit', function(e) {
+    e.preventDefault();
+    const vvod = $('#vvvod').val();
+    const vvod2 = $('#vvvod2').val();
+
+
+
+    if (vvod == '' || vvod2 =='') {
+        document.getElementById("nameErrMsg").innerHTML = " Введіть щось в поля";
+        document.getElementById("vvvod").setAttribute('style', 'border: 2px solid #ff0000');
+        document.getElementById("vvvod2").setAttribute('style', 'border: 2px solid #ff0000');
+        return false
+    }
+
+    fetch(`http://10.30.1.208:8080/nais-exchange/resources/cars/RegisterVehiclesOwners/GetTechnicalPassport/${vvod}/${vvod2}`)
+
+        .then(response => {
+
+        if (response.ok) {
+        return response.json();
+
+    }else {
+
+        return window.location.href='http://hsc.gov.ua/svidocnotfound/';
+    }
+})
+.then(renderHTML)
+
+        .catch(err => console.log('Error, with message:', err.statusText))
+})
+
+/* $("#btn").on('touchstart click', function(){
+
+     e.preventDefault();
+     const vvod = $('#vvvod').val();
+     const vvod2 = $('#vvvod2').val();
+
+     fetch(`http://10.30.1.208:8080/nais-exchange/resources/cars/RegisterVehiclesOwners/GetTechnicalPassport/${vvod}/${vvod2}`)
+
+       .then(response => {
+
+         if (response.ok) {
+             return response.json();
+
+         }else {
+
+            return window.location.href='http://hsc.gov.ua/svidocnotfound/';
+         }
+        })
+         .then(renderHTML)
+
+         .catch(err => console.log('Error, with message:', err.statusText))
+
+ })*/
+
+/*var flag = false;
+$("#btn").bind('touchstart click', function(){
+  if (!flag) {
+    flag = true;
+    setTimeout(function(){ flag = false; }, 300);
+    const vvod = $('#vvvod').val();
+    const vvod2 = $('#vvvod2').val();
+
+    fetch(`http://10.30.1.208:8080/nais-exchange/resources/cars/RegisterVehiclesOwners/GetTechnicalPassport/${vvod}/${vvod2}`)
+
+      .then(response => {
+
+        if (response.ok) {
+            return response.json();
+
+        }else {
+
+           return window.location.href='http://hsc.gov.ua/svidocnotfound/';
         }
-    };
-    myRequest.send();
+       })
+        .then(renderHTML)
 
-});
+        .catch(err => console.log('Error, with message:', err.statusText))
+  }
+
+  return false
+});*/
+
+
+/* $(document).ready(function() {
+     $("#btnSubmit").click(function(){
+          const vvod = $('#vvvod').val();
+         const vvod2 = $('#vvvod2').val();
+
+         fetch(`http://10.30.1.208:8080/nais-exchange/resources/cars/RegisterVehiclesOwners/GetTechnicalPassport/${vvod}/${vvod2}`)
+
+           .then(response => {
+
+             if (response.ok) {
+                 return response.json();
+
+             }else {
+
+                return window.location.href='http://hsc.gov.ua/svidocnotfound/';
+             }
+            })
+             .then(renderHTML)
+
+             .catch(err => console.log('Error, with message:', err.statusText))
+
+
+     });
+ });
+*/
+
+/*$(document).ready(function() {
+            window.onload = function(){
+                btn.addEventListener("click", function () {
+                    const vvod = $('#vvvod').val();
+                    const vvod2 = $('#vvvod2').val();
+
+
+
+                    if (vvod == '' || vvod2 =='') {
+                           document.getElementById("nameErrMsg").innerHTML = " Введіть щось в поля";
+                           document.getElementById("vvvod").setAttribute('style', 'border: 2px solid #ff0000');
+                           document.getElementById("vvvod2").setAttribute('style', 'border: 2px solid #ff0000');
+                           return false
+                    }
+
+                    fetch(`http://10.30.1.208:8080/nais-exchange/resources/cars/RegisterVehiclesOwners/GetTechnicalPassport/${vvod}/${vvod2}`)
+
+                      .then(response => {
+
+                        if (response.ok) {
+                            return response.json();
+
+                        }else {
+
+                           return window.location.href='http://hsc.gov.ua/svidocnotfound/';
+                        }
+                       })
+                        .then(renderHTML)
+
+                        .catch(err => console.log('Error, with message:', err.statusText))
+
+                        return false;
+
+                });
+            };
+        });*/
+
+const keys = {
+    nRegNew: {
+        field1: `A`,
+        field2: `Реєстраційний номер:`
+    },
+    dFirstReg: {
+        field1: `B`,
+        field2: `Дата першої реєстрації:`
+    },
+    dReg: {
+        field1: `B.1`,
+        field2: `Дата реєстрації:`
+    },
+    makeYear: {
+        field1: `B.2`,
+        field2: `Рік випуску:`
+    },
+    brand: {
+        field1: `D.1`,
+        field2: `Марка:`
+    },
+    model: {
+        field1: `D2`,
+        field2: `Модель:`
+    },
+    kind: {
+        field1: `D3`,
+        field2: `Тип:`
+    },
+    vin: {
+        field1: `E`,
+        field2: `Номер шасі (кузова, рами):`
+    },
+    totalWeight: {
+        field1: `F.1`,
+        field2: `Повна маса :`
+    },
+    ownWeight: {
+        field1: `G`,
+        field2: `Маса без навантаження :`
+    },
+    rankCategory: {
+        field1: `J`,
+        field2: `Категорія :`
+    },
+    capacity: {
+        field1: `P.1`,
+        field2: `Об'єм двигуна:`
+    },
+    fuel: {
+        field1: `P.2`,
+        field2: `Тип палива:`
+    },
+    color: {
+        field1: `R`,
+        field2: `Колір:`
+    },
+    nSeating: {
+        field1: `S.1`,
+        field2: `Кіль-сть сидячих місць з місцем водія:`
+    },
+    nStanding: {
+        field1: `S.2`,
+        field2: `Кіль-сть стоячих місць:`
+    },
+};
 
 function renderHTML(data) {
 
-    var htmlString = "";
+    if (data && data.length) {
 
-    for (var i = 0; i < data.length; i++) {
-        var htmlString;
-        var obj = Object.keys(data[i]);
-        for(var j = 0; j < Object.keys(data[i]).length; j++) {
-            var propertyName = obj[j]
-            htmlString += renderDiv(propertyName, data[i][propertyName])
-        }
+        brr = document.getElementById('second');
+        brr2 = document.getElementById('first');
+        $(brr2).hide(1000);
+        $(brr).show(1000);
 
+        /* document.getElementById('try_hide' ).style.display = 'block';*/
+        /* document.getElementById('try_hide_first').style.display = 'none';*/
+        let html = `<div class="container_out">`;
 
-        // htmlString += "<p>" + "Дата народження: " + data[i].birthday  + "<br>"
-        //                     + "Тип кузова: " + data[i].body + "<br>"
-        // 					+ "Марка-модель: " + data[i].brand + "<br>"
-        // 					+ "Об'єм двигуна: " + data[i].capacity + "<br>"
-        // 					+ "Адреса: " + data[i].clntAddress + "<br>"
-        // 					+ "ІД: " + data[i].clntId + "<br>"
-        // 					+ "Колір: " + data[i].color + "<br>"
-        // 					+ "Дата реєстрації: " + data[i].dReg + "<br>"
-        // 					+ "ТСЦ №: " + data[i].dep + "<br>"
-        // 					+ "Док ІД: " + data[i].docId + "<br>"
-        // 					+ "Тип палива: " + data[i].fuel + "<br>"
-        // 					+ "ІНН: " + data[i].innChar + "<br>"
-        // 					+ "Тип ТЗ за конструкцією: " + data[i].kind + "<br>"
-        // 					+ "Рік випуску: " + data[i].makeYear + "<br>"
-        // 					+ "Номер кузова: " + data[i].nBody + "<br>"
-        // 					+ "Номер двигуна: " + data[i].nEngine + "<br>"
-        // 					+ "Номерний знак: " + data[i].nRegNew + "<br>"
-        // 					+ "П.І.Б. власника: " + data[i].name + "<br>"
-        // 					+ "Номер свідоцтва: " + data[i].newCert + "<br>"
-        // 					+ "НЗА: " + data[i].nza + "<br>"
-        // 					+ "Операція реєстрації: " + data[i].operName + "<br>"
-        // 					+ "Власна вага: " + data[i].ownWeight + "<br>"
-        // 					+ "Тип ТЗ за призначенням: " + data[i].purpose + " прідурак" + "<br>"
-        // 					+ "Загальна вага: " + data[i].totalWeight + "<br>"
-        // 					+ "VIN авто: " + data[i].vin + "<br>"	+	".</p>";
+        data.forEach(item => {
+            const keysKeys = Object.keys(keys);
+        keysKeys.forEach(key => {
+            /*const lastDiv = (item[key]) ? item[key] : '?'*/
+            html += `<div class="rowContainer">
+                            <div class="firstDiv">${keys[key].field1}</div>
+                            <div class="secondDiv">${keys[key].field2}</div>
+                            <div class="lastDiv">${(item[key]) ? item[key] : '?'}</div>
+                        </div>`
+    });
+    });
+
+        html += `</div>`;
+
+        infoContainer.insertAdjacentHTML('beforeend', html);
     }
 
-    infoContainer.insertAdjacentHTML('beforeend', htmlString);
-}
 
-function renderDiv(propertyName, value) {
-    var divString = "<div id=" + propertyName + ">" + value + "</div> <br>"
-    return divString;
 }
-
